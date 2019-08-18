d13c4cb8de186fd0a338e92054c1341979f6daee
Littlesqx <Littlesqx@gmail.com>
2019-08-19 00:01:37 +0800

    Add exec entry

---
+++ src/Bin/handle.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * This file is part of aint-queue.
+ *
+ * Copyright © 2012 - 2019 Xiaoman. All Rights Reserved.
+ *
+ * Created by Shengqian <shengqian@xiaoman.cn>, on 2019/08/16.
+ */
