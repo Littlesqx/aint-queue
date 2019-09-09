9200d800fc6a0b9a624bc684ffd95e644994c369
Littlesqx <Littlesqx@gmail.com>
2019-09-09 09:49:08 +0800

    Format

--- example/project/public/index.php
+++ example/project/public/index.php
@@ -1,22 +1,22 @@
 <?php
 
-/**
- * This file is part of aint-queue.
+/*
+ * This file is part of the littlesqx/aint-queue.
  *
- * Copyright © 2012 - 2019 Xiaoman. All Rights Reserved.
+ * (c) littlesqx <littlesqx@gmail.com>
  *
- * Created by Shengqian <shengqian@xiaoman.cn>, on 2019/09/06.
+ * This source file is subject to the MIT license that is bundled.
  */
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
-use Littlesqx\AintQueue\Driver\DriverFactory;
-use Littlesqx\AintQueue\Driver\Redis\Queue;
-use App\Job\SyncJob;
 use App\Job\AsyncJob;
 use App\Job\CoJob;
+use App\Job\SyncJob;
+use Littlesqx\AintQueue\Driver\DriverFactory;
+use Littlesqx\AintQueue\Driver\Redis\Queue;
 
-$config = require __DIR__ . '/../config/aint-queue.php';
+$config = require __DIR__.'/../config/aint-queue.php';
 
 $channel = 'example';
 $driverOption = $config[$channel] ?? [];
@@ -31,9 +31,4 @@ $queue->push(new SyncJob());
 $queue->push(new AsyncJob());
 $queue->push(new CoJob());
 
-
 header('HTTP/1.1 201 Created');
-
-
-
-
